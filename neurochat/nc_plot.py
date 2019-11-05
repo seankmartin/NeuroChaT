0f4fc678a4af8a361feabddc687ae53507713fa4
Sean Martin <martins7@tcd.ie>
2019-11-05 16:40:29 +0000

    Update HD plot in place summary

--- neurochat/nc_plot.py
+++ neurochat/nc_plot.py
@@ -2129,8 +2129,15 @@ def print_place_cells(
             if headdata[i] is not None:
                 idx = get_mapping_idx("HD")
                 if idx is not None:
+                    hd_data = headdata[i]
                     ax = fig.add_subplot(gs[j, idx], projection='polar')
-                    hd_rate(headdata[i], ax=ax, title=None)
+                    hd_rate(hd_data, ax=ax, title=None)
+                    bins = np.append(hd_data['bins'], hd_data['bins'][0])
+                    predRate = np.append(
+                        hd_data['hdPred'], hd_data['hdPred'][0])
+                    ax.plot(np.radians(bins), predRate, color='green')
+                    ax.set_rticks(
+                        [hd_data['hdRate'].max(), hd_data['hdPred'].max()])
 
         
         if wavedata is not None:
