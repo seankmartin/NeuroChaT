06e6e60269b2704d5e02ce882f45a44a063d831c
Sean Martin <seankieran.m@hotmail.com>
2020-06-15 12:08:21 +0100

    Added Sphinx docs

---
+++ docs/conf.py
@@ -0,0 +1,68 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+import os
+import sys
+curdir = os.path.dirname(__file__)
+package_location = os.path.abspath(
+    os.path.join(curdir, '../'))
+sys.path.append(package_location)
+
+# -- Project information -----------------------------------------------------
+
+project = 'NeuroChaT'
+copyright = '2020, Md Nurul Islam, Sean Martin'
+author = 'Md Nurul Islam, Sean Martin'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0.2'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinxcontrib.apidoc'
+]
+
+# -- Apidoc configuration ----------------------------------------------------
+apidoc_module_dir = '../neurochat'
+apidoc_output_dir = 'reference'
+apidoc_excluded_paths = ['tests']
+apidoc_separate_modules = True
+
+# -- Napoleon settings -------------------------------------------------------
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = False
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. NeuroChaT documentation master file, created by
+   sphinx-quickstart on Mon Jun 15 11:07:10 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+NeuroChaT - Neuron Characterisation Toolbox
+===========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   reference/modules
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
